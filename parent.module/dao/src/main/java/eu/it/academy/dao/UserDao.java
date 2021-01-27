5b268eefb199faaea64b981b1e46cfa2bc63b879
vydraoleg <70654612+vydraoleg@users.noreply.github.com>
2021-01-27 22:35:40 +0300

    123123

--- parent.module/dao/src/main/java/eu/it/academy/dao/UserDao.java
+++ parent.module/dao/src/main/java/eu/it/academy/dao/UserDao.java
@@ -3,7 +3,9 @@ package eu.it.academy.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import eu.it.academy.entity.User;
+
 public class UserDao {
-    private List<User>  users = new ArrayList<>();
+    private List<User> users = new ArrayList<>();
     
 }
