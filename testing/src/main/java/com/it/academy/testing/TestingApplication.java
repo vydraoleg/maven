24aa660818ecedccefdf344046129acffed1b4db
Oleg <vydraoleg@gmail.com>
2021-02-10 19:03:36 +0300

    123213
    123123

--- testing/src/main/java/com/it/academy/testing/TestingApplication.java
+++ testing/src/main/java/com/it/academy/testing/TestingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestingApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TestingApplication.class, args);
 	}
 
